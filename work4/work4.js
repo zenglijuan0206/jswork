3e877083804f656e6c047b44e75734521618ceb4
zenglijuan0206 <2316268389@qq.com>
2019-10-13 10:41:10 +0800

    work4

--- work4/work4.js
+++ work4/work4.js
@@ -4,8 +4,8 @@ level =parseFloat(level)&&Number(level)
 if (isNaN(level)){
     alert('金字塔的层数是数字')
 }
-for(let i=;i<=level;++i){
-    let blank =level -i;
+for(let i=1;i<=level;++i){
+    let blank = level - i;
     for(let k=0;k<blank;++k){
         document.write('&nbsp;');
     }
@@ -13,13 +13,13 @@ let star = i * 2 - 1
 for(let j=0;j<star;++j){
     document.write('*')
 }
-document.write('<br>')
+document.write("<br>")
 }
-let str ='<table border="1">1'
+let str ='<table border="1">'
 for(let i=1;i<10;++i){
-    str += '<br>'
+    str += '<tr>'
     for(var j=1;j<=i;++j){
-        str +='<td> + j+'*'+ i + '=∫'+(j * i) + '</td>
+       str += '<td>' + j + '*' + i + '='+ (j * i) +'</td>'
     }
 str +='</tr>'
 }
