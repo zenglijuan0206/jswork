ea1fc1c0c5ef93f43d7c992eab60271db2eb297e
zenglijuan0206 <2316268389@qq.com>
2019-10-21 13:36:07 +0800

    work7

--- work7/index.js
+++ work7/index.js
@@ -1,12 +1,12 @@
-function sort(methed){
-    let origin = document.getElementById('origin').value
+function sort(method){
+    let origin =document.getElementById('origin').value 
     let logs
-    console.log(methed)
-    if(methed=='bubble'){
-        logs = bubbleSort(origin)
-    }else if(methed=='insert'){
-        log = insertSort(origin)
+    console.log(method)
+    if(method=='bubble'){
+        logs =bubbleSort(origin)
+    }else if(method=='insert'){
+        logs = insertSort(origin)
     }
-    showDiv = document.getElementById('Anim')
+    showDiv =document.getElementById('Anim')
     showDiv.innerHTML = logs
 }
