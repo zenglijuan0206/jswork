5af22323876b3d9a3b51c82474bf6af9855b7e2f
zenglijuan0206 <2316268389@qq.com>
2019-12-23 09:57:51 +0800

    work18

---
+++ work18/index.js
@@ -0,0 +1,26 @@
+function send() {
+    let sno = document.getElementById('sno').value
+    let name = document.getElementById('name').value
+    let content = document.getElementById('content').value
+    let xmlhttp;
+    if(window.XMLHttpRequest){
+
+        xmlhttp = new XMLHttpRequest();
+    }else{
+
+        xmlhttp = new ActiveXObject('microsoft.XMHLTTP')
+    }
+
+    
+    xmlhttp.open('post','http://139.9.81.283:8090/ajax',true)
+    xmlhttp.setRequestHeader("Content-type","application/x-www-from-urlencoded");
+    xmlhttp.send('sno=' + sno + '&name' + name+"&content="+content)
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState ==4 && xmlhttp.status == 200){
+            let ajaxData=JSON.parse(xmlhttp.responseText)
+            result = ajaxData.reverse().map((val)=>{return JSON.stringify(val)})
+            document.getElementById("result").innerHTML = result.join('<br>')
+    }
+    }
+}
